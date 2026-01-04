2e0469f455165a94bef4cf33af3af73cf63096f1
Naresh <naresh.devops2025@gmail.com>
2026-01-05 00:05:01 +0530

    2nd commit--added config files

---
+++ application/src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World from Kubernetes";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
--- application/src/main/java/example/helloworld/HelloWorldApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.helloworld;
-
-public class HelloWorldApplication {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-    }
-}
