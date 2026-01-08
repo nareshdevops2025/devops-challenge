c6ebb6421fa3b80e6744eb5c1ff73296b34b65c9
Naresh <naresh.devops2025@gmail.com>
2026-01-08 20:20:27 +0530

    for k8s

--- application/src/main/java/com/example/hello/HelloApplication.java
+++ application/src/main/java/com/example/hello/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 @RestController
